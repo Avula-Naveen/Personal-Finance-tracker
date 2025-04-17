878931c751645dd800bba228a6e1631e99248c31
Avula Naveen <avulanaveen101@gmail.com>
2025-04-17 13:34:49 +0530

    Fix component import case for Vercel

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -1,7 +1,8 @@
 
 import { useState } from "react";
-import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/Input";
+import { Button } from "../components/ui/Button";
+
 import AddButton from './AddButton';
 
 const categories = ["Food", "Transport", "Health", "Entertainment", "Other"];
